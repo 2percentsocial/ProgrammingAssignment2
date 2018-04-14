21a67002c7ee55f42b1025031658b8b848af942d
Roberto <roberto.m.gonzal@gmail.com>
2018-04-14 00:52:54 -0500

    Inverse of a Matrix
    If inverse of matrix is already stored in memory uses cached matrix, if not then solves inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions solve the inverse of matrix if it's not already saved in memory
 
-## Write a short comment describing this function
+## Creates a list of the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Checks if inverse matrix is already in cache, if not solves inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
